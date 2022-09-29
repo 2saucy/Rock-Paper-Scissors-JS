ca7f48e0ca04333eaeeb2060f68eada3d7935026
2.saucy <geronimomembiela@gmail.com>
2022-09-29 19:06:39 -0300

    edit variable names, add functions , coments and styles

--- scripts.js
+++ scripts.js
@@ -6,38 +6,32 @@ const cScore = document.querySelector('.c-score');
 const modalText = document.querySelector('.modal-text');
 const playerChoice = document.querySelector('.player-choice');
 const computerChoice = document.querySelector('.computer-choice');
+
+/* global variables */
 let playerScore = 0;
 let computerScore = 0;
 
-
-function getComputerChoice(){
+/* get random choice computer side */
+function getComputerChoice() {
     let choices = [
         'Rock',
         'Paper',
         'Scissor'
     ]
-    let random = Math.floor(Math.random()*choices.length);
+    let random = Math.floor(Math.random() * choices.length);
     return choices[random]
 }
 
-function playRound(playerSelection, computerSelection){
+/* returns the result string and sum score*/
+function roundResult(playerSelection, computerSelection) {
+    displayChoices(playerSelection, computerSelection);
+    operation = (playerSelection == 'Rock' && computerSelection == 'Scissor') || (playerSelection == 'Paper' && computerSelection == 'Rock') || (playerSelection == 'Scissor' && computerSelection == 'Paper')
 
-    playerChoice.setAttribute('src', `./images/${playerSelection}.png`);
-    computerChoice.setAttribute('src', `./images/${computerSelection}-pc.png`);
-
-    if (playerSelection == 'Rock' && computerSelection == 'Scissor'){
-        playerScore ++;
+    if (operation) {
+        playerScore++;
         return `You Win! ${playerSelection} beats ${computerSelection}.`
     }
-    else if (playerSelection == 'Paper' && computerSelection == 'Rock'){
-        playerScore ++;
-        return `You Win! ${playerSelection} beats ${computerSelection}.`
-    }
-    else if (playerSelection == 'Scissor' && computerSelection == 'Paper'){
-        playerScore ++;
-        return `You Win! ${playerSelection} beats ${computerSelection}.`
-    }
-    else if(playerSelection == computerSelection){
+    else if (playerSelection == computerSelection) {
         return `Draw! both chose ${computerSelection}.`
     }
     else {
@@ -46,30 +40,44 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-function winnerReport(){
-    if (playerScore > computerScore){
+/* play a round and shows the result */
+function playRound(choice) {
+    round = roundResult(choice, getComputerChoice())
+    resultText.textContent = round;
+}
+
+/* set the src based on what the player and the computer chose */
+function displayChoices(playerSelection, computerSelection) {
+    playerChoice.setAttribute('src', `./images/${playerSelection}.png`);
+    computerChoice.setAttribute('src', `./images/${computerSelection}-pc.png`);
+}
+
+/* winner msg and unhide the modal */
+function modalMessage() {
+    if (playerScore > computerScore) {
         modalText.textContent = 'Sheeesh, you are the winner. Congratulations! ';
     }
-    else{
-        modalText.textContent = 'What a shame, you lose this game. ';
+    else {
+        modalText.textContent = 'What a shame, you lose this time. ';
     }
-    if(playerScore >= 5 || computerScore >= 5){
+    if (playerScore >= 5 || computerScore >= 5) {
         modal.style.display = "block";
     }
 }
 
-function game(choice){
-    round = playRound(choice, getComputerChoice())
-    resultText.textContent = round;
+/* set both scores */
+function displayScore(playerScore, computerScore) {
+    pScore.textContent = playerScore;
+    cScore.textContent = computerScore;
 }
 
+
 buttons.forEach((button) => {
-    button.addEventListener('click', function (e){
-        game(button.attributes[1].value)
-        pScore.textContent = playerScore;
-        cScore.textContent = computerScore;
-        if (playerScore == 5 || computerScore == 5){
-            winnerReport();
+    button.addEventListener('click', function (e) {
+        playRound(button.attributes[1].value)
+        displayScore(playerScore, computerScore);
+        if (playerScore == 5 || computerScore == 5) {
+            modalMessage();
         }
-});
+    });
 });
