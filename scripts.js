8203495cb98485ee6ea3dbc816f56f055f8c7197
2.saucy <geronimomembiela@gmail.com>
2022-09-29 18:03:27 -0300

    add readme, change variables names

--- scripts.js
+++ scripts.js
@@ -1,14 +1,15 @@
 const modal = document.querySelector('.modal')
-const buttons = document.querySelectorAll('.img-choices');
-const results = document.querySelector('.table-score');
-const p_result = document.querySelector('.result-text')
-const p_score = document.querySelector('.p-score');
-const c_score = document.querySelector('.c-score');
-const modal_text = document.querySelector('.modal-text');
+const buttons = document.querySelectorAll('.btn-player');
+const resultText = document.querySelector('.result-text')
+const pScore = document.querySelector('.p-score');
+const cScore = document.querySelector('.c-score');
+const modalText = document.querySelector('.modal-text');
 const playerChoice = document.querySelector('.player-choice');
 const computerChoice = document.querySelector('.computer-choice');
 let playerScore = 0;
 let computerScore = 0;
+
+
 function getComputerChoice(){
     let choices = [
         'Rock',
@@ -18,6 +19,7 @@ function getComputerChoice(){
     let random = Math.floor(Math.random()*choices.length);
     return choices[random]
 }
+
 function playRound(playerSelection, computerSelection){
 
     playerChoice.setAttribute('src', `./images/${playerSelection}.png`);
@@ -43,26 +45,29 @@ function playRound(playerSelection, computerSelection){
         return `You Lose! ${computerSelection} beats ${playerSelection}.`
     }
 }
+
 function winnerReport(){
     if (playerScore > computerScore){
-        modal_text.textContent = 'Sheeesh, you are the winner. Congratulations! ';
+        modalText.textContent = 'Sheeesh, you are the winner. Congratulations! ';
     }
     else{
-        modal_text.textContent = 'What a shame, you lose this game. ';
+        modalText.textContent = 'What a shame, you lose this game. ';
     }
     if(playerScore >= 5 || computerScore >= 5){
         modal.style.display = "block";
     }
 }
+
 function game(choice){
     round = playRound(choice, getComputerChoice())
-    p_result.textContent = round;
+    resultText.textContent = round;
 }
+
 buttons.forEach((button) => {
     button.addEventListener('click', function (e){
         game(button.attributes[1].value)
-        p_score.textContent = playerScore;
-        c_score.textContent = computerScore;
+        pScore.textContent = playerScore;
+        cScore.textContent = computerScore;
         if (playerScore == 5 || computerScore == 5){
             winnerReport();
         }
