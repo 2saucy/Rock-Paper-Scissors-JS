67c77b95d09717aaeee6a83a7158107df976bcd7
2.saucy <geronimomembiela@gmail.com>
2022-09-29 03:39:33 -0300

    finished ui

--- scripts.js
+++ scripts.js
@@ -1,56 +1,70 @@
+const modal = document.querySelector('.modal')
+const buttons = document.querySelectorAll('.img-choices');
+const results = document.querySelector('.table-score');
+const p_result = document.querySelector('.result-text')
+const p_score = document.querySelector('.p-score');
+const c_score = document.querySelector('.c-score');
+const modal_text = document.querySelector('.modal-text');
+const playerChoice = document.querySelector('.player-choice');
+const computerChoice = document.querySelector('.computer-choice');
+let playerScore = 0;
+let computerScore = 0;
 function getComputerChoice(){
     let choices = [
-        'rock',
-        'paper',
-        'scissor'
+        'Rock',
+        'Paper',
+        'Scissor'
     ]
     let random = Math.floor(Math.random()*choices.length);
     return choices[random]
 }
-
 function playRound(playerSelection, computerSelection){
-    if (playerSelection == 'rock' && computerSelection == 'scissor'){
+
+    playerChoice.setAttribute('src', `./images/${playerSelection}.png`);
+    computerChoice.setAttribute('src', `./images/${computerSelection}-pc.png`);
+
+    if (playerSelection == 'Rock' && computerSelection == 'Scissor'){
+        playerScore ++;
         return `You Win! ${playerSelection} beats ${computerSelection}.`
     }
-    else if (playerSelection == 'paper' && computerSelection == 'rock'){
+    else if (playerSelection == 'Paper' && computerSelection == 'Rock'){
+        playerScore ++;
         return `You Win! ${playerSelection} beats ${computerSelection}.`
     }
-    else if (playerSelection == 'scissor' && computerSelection == 'paper'){
+    else if (playerSelection == 'Scissor' && computerSelection == 'Paper'){
+        playerScore ++;
         return `You Win! ${playerSelection} beats ${computerSelection}.`
     }
     else if(playerSelection == computerSelection){
-        return `Draw! both chose ${computerSelection}`
+        return `Draw! both chose ${computerSelection}.`
     }
     else {
+        computerScore++;
         return `You Lose! ${computerSelection} beats ${playerSelection}.`
     }
 }
-
-function game(){
-    let playerSelection, computerSelection, playerScore=0, computerScore=0, round;
-
-    /* Loop 5 rounds */
-    for(i=1; i<=5; i++){
-        playerSelection = prompt("Rock, paper or scissor?: ");
-        computerSelection = getComputerChoice();
-        round = playRound(playerSelection.toLowerCase(), computerSelection)
-        console.log(round);
-        if (round.includes('Win')){
-            playerScore += 1
-        }
-        else if (round.includes('Lose')){
-            computerScore += 1
-        }    
-    }
-
-    /* Winner Report */
+function winnerReport(){
     if (playerScore > computerScore){
-        console.log('Sheeesh, you are the winner. Congratulations! ')
-    } else if (playerScore < computerScore){
-        console.log('What a shame, you lose this game. ')
-    }else {
-        console.log('This is an unexpected draw. Nice try, i guess.')
+        modal_text.textContent = 'Sheeesh, you are the winner. Congratulations! ';
+    }
+    else{
+        modal_text.textContent = 'What a shame, you lose this game. ';
+    }
+    if(playerScore >= 5 || computerScore >= 5){
+        modal.style.display = "block";
     }
 }
-
-game()
+function game(choice){
+    round = playRound(choice, getComputerChoice())
+    p_result.textContent = round;
+}
+buttons.forEach((button) => {
+    button.addEventListener('click', function (e){
+        game(button.attributes[1].value)
+        p_score.textContent = playerScore;
+        c_score.textContent = computerScore;
+        if (playerScore == 5 || computerScore == 5){
+            winnerReport();
+        }
+});
+});
